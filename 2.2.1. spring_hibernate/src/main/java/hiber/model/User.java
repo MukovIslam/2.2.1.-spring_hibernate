de6dc3af24ded17367bebfa17178662ba60d4943
MukovIslam <124084552+MukovIslam@users.noreply.github.com>
2024-05-19 12:19:37 +0300

    Add files via upload

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -0,0 +1,90 @@
+package hiber.model;
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(name = "last_name")
+   private String lastName;
+
+   @Column(name = "name")
+   private String firstName;
+
+
+
+   @Column(name = "email")
+   private String email;
+   @Column(name = "car_id")
+
+   private Long car_id;
+
+   @OneToOne(mappedBy = "user")
+   @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+   private Car car;
+
+   public User() {}
+
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+      car.setUser(this);
+   }
+
+   public Long getCar_id() {
+      return car_id;
+   }
+
+   public void setCar_id(Long car_id) {
+      this.car_id = car_id;
+   }
+}
